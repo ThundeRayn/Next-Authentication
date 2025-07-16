a27980c041fbd06c200a36fe35b9135606620e14
Your Name <tangshirong477@gmail.com>
2025-07-16 15:02:37 -0400

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from "next/link";
+
+const UsersPage = () => {
+  return (
+    <div>
+      <Link href="/new">UsersPage</Link>
+    </div>
+  )
+}
+
+export default UsersPage
